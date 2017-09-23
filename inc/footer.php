74f4b33ce3cc3422be6336caa11a1e5109bdd26f
RicardoFernandesSouza <ricardo_fernandes.souza@hotmail.com>
2017-09-23 19:12:29 -0300

    Ajustes no footer de GITHUB para 2017

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
 	
 	<hr>
 	<footer class="container">
-		<p>&copy;2017 - To Home GITHUB</p>
+		<p>&copy;2017 - To Home 2017</p>
 	</footer>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
