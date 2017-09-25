97dd43dd98d87cd4981e52a04f8d992e6200405e
Adriano Borella <adrianoborella@yahoo.com.br>
2017-09-25 16:57:19 -0300

    Formatacao do ceb no cadastro e edicao de Residencia

--- residencia/add.php
+++ residencia/add.php
@@ -3,6 +3,19 @@
   add();
 ?>
 
+<script>
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+</script>
+
 <?php include(HEADER_TEMPLATE); ?>
 
 <h2>Nova Residência</h2>
@@ -23,7 +36,7 @@
     
     <div class="form-group col-md-3">
       <label for="campo3">CEP</label>
-      <input type="text" class="form-control" name="residencia['zip_code']" required>
+      <input type="text" class="form-control" name="residencia['zip_code']" maxlength="9" OnKeyPress="formatar('#####-###', this)" required>
     </div>
   
     <div class="form-group col-md-3">
