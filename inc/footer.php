606232fe5e3dccee35b1d35ec54a582cebdeafdd
Adriano Borella <adrianoborella@yahoo.com.br>
2017-10-09 20:06:21 -0300

    Removido o Jquery do footer para exibir icones edicao das etapas

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 </main> <!-- /container -->
 	
 	<style>
-	<?php include 'footer.css'; ?>
+	<?php //include 'footer.css'; ?>
 	</style>
 	
 	<hr>
@@ -9,9 +9,7 @@
 		<p>&copy;2017 - To Home 2017</p>
 	</footer>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo BASEURL; ?>js/jquery-1.11.2.min.js"><\/script>')</script>
-
+	
     <script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>
 
     <script src="<?php echo BASEURL; ?>js/main.js"></script>
