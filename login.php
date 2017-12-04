f905c73414e419912d72b8b9d9f00e6190ffc72b
Adriano Borella <adrianoborella@yahoo.com.br>
2017-12-04 11:03:46 -0200

    Adicionado logo na tela de login

--- login.php
+++ login.php
@@ -8,31 +8,26 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="Login ToHome">
+       <meta name="description" content="Login ToHome">
     <meta name="author" content="ToHome">
     <link rel="icon" href="imagens/favicon.ico">
 
     <title>Login ToHome</title>
-
+      <div align="center">
+        <img src="logo.png"
+             width="300"
+             height="300"
+             align="bottom" />
+      </div>
     <!-- Bootstrap core CSS -->
     <link href="css/login_bootstrap.min.css" rel="stylesheet">
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
     <link href="css/login_signin.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/login_ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -40,7 +35,7 @@ session_start();
     <div class="container">
 
       <form class="form-signin" method="POST" action= 'includes/login.inc.php'>
-        <h2 class="form-signin-heading text-center">To Home</h2>
+        <!--<h2 class="form-signin-heading text-center">To Home</h2>-->
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="text" name="uid" class="form-control" placeholder="Usuário" required autofocus><br />
         <label for="inputPassword" class="sr-only">Password</label>
@@ -52,10 +47,9 @@ session_start();
         <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
       </form>
 
-    </div> <!-- /container -->
+    </div>
 
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
--- resp/add.php
+++ resp/add.php
@@ -5,18 +5,6 @@
 
 <?php include(HEADER_TEMPLATE); ?>
 
-<script>
-function formatar(mascara, documento){
-  var i = documento.value.length;
-  var saida = mascara.substring(0,1);
-  var texto = mascara.substring(i)
-  
-  if (texto.substring(0,1) != saida){
-            documento.value += texto.substring(0,1);
-  }
-  
-}
-</script>
 
 <h2>Novo Responsável</h2>
 
